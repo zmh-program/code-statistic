71663570eb96ae64971e038f4a1906d50eebb964
Zhang Minghan <zmh13054618081@dingtalk.com>
2023-02-11 10:33:28 +0800

    0.2

--- index.js
+++ index.js
@@ -1,21 +1,44 @@
 const express = require('express');
 const axios = require('axios');
+const token = process.env.CODE_STATISTIC || "";
 const app = express();
 
 async function syncAxios(url) {
-    return (await axios.get(url)).data;
+    return (await axios.get(url, {
+        headers: {
+            Accept: "application/json",
+            Authorization: `Bearer ${token}`,
+        }
+    })).data;
 }
 
 async function getLanguage(user, repo) {
     return await syncAxios(`https://api.github.com/repos/${user}/${repo}/languages`);
 }
+
+async function langStatistics(queue) {
+    const res = {};
+    for (const idx in queue) {
+        const task = queue[idx];
+        if (task instanceof Promise) {
+            const task_response = await task;
+            for (const lang in task_response) {
+                lang in res ?
+                  res[lang] += task_response[lang] :
+                  res[lang] = task_response[lang];
+            }
+        }
+    }
+    return res;
+}
+
 app.get('/user/:user/', async function (req, res) {
     const username = req.params.user;
     const response = await syncAxios(`https://api.github.com/users/${username}/repos`);
-    const statistic = Object.values(response).map(async (resp) => {
+    const result = await langStatistics(Object.values(response).map(async (resp) => {
         return await getLanguage(username, resp['name']);
-    });
-    res.send(statistic);
+    }));
+    res.send(result);
 })
 
 app.get('/repo/:user/:repo/', function (req, res) {
