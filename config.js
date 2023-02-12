335308d98a488d55c725b9e2bab629e8d00be4ac
Zhang Minghan <112773885+zmh-program@users.noreply.github.com>
2023-02-12 15:19:37 +0000

    1.2

--- config.js
+++ config.js
@@ -2,5 +2,5 @@ module.exports = {
   token: process.env.CODE_STATISTIC, // GitHub Access Token (Minimum permissions). Increase QPS of GitHub APIs.
   expiration: 3600,  // expiration second
   requires: ["*"], // CODE STATISTIC can only be parsed for allowed users. ( * indicates that all users are allowed )
-  port: 8000, // server port
+  port: 80, // server port
 }
