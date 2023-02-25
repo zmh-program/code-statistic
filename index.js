159cc200c91ed797d163e680b74deaf282db2a4c
Zhang Minghan <zmh13054618081@dingtalk.com>
2023-02-25 23:40:30 +0800

    1.10

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ app.set('views',__dirname + '/views');
 app.set("view engine","ejs");
 
 
-const isAvailableUser = (username) => {
-    return ( !! username ) && ( conf.requires.includes("*") || conf.requires.includes(username));
+const notAccess = (username) => {
+    return ( ! username ) || (! ( conf.requires.includes("*") || conf.requires.includes(username)));
 }
 
 app.get('/', function(req, res) {
@@ -18,21 +18,27 @@ app.get('/', function(req, res) {
 })
 
 app.get('/user/:user/', async function (req, res) {
-    const username = req.params['user'];
+  res.type('svg');
 
-    res.type('svg');
-    isAvailableUser(username) ?
-      res.render('user', await stats.getAccount(username, req.query['theme'] === 'dark')):
-      res.render('error', {dark: req.query['theme'] === 'dark'});
+  try {
+    const username = req.params['user'];
+    if (notAccess(username)) throw new Error();
+    res.render('user', await stats.getAccount(username, req.query['theme'] === 'dark'));
+  } catch (e) {
+    res.render('error', {dark: req.query['theme'] === 'dark'});
+  }
 });
 
 app.get('/repo/:user/:repo/', async function (req, res) {
+  res.type('svg');
+  try {
     const username = req.params['user'], repo = req.params['repo'];
 
-    res.type('svg');
-    isAvailableUser(username) && repo.length ?
-      res.render('repo', await stats.getRepository(username, repo, req.query['theme'] === 'dark'))
-      : res.render('error', {dark: req.query['theme'] === 'dark'});
+    if (notAccess(username) || (!repo.length)) throw new Error();
+    res.render('repo', await stats.getRepository(username, repo, req.query['theme'] === 'dark'))
+  } catch (e) {
+    res.render('error', {dark: req.query['theme'] === 'dark'});
+  }
 });
 
 
