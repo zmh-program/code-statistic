521add025e6e1724ea3e11ba03b6c5439f4bd913
Zhang Minghan <zmh13054618081@dingtalk.com>
2023-06-06 19:14:08 +0800

    update frontend

--- gui/src/App.vue
+++ gui/src/App.vue
@@ -1,36 +1,97 @@
+<script setup lang="ts">
+</script>
 <template>
-  <t-config-provider :global-config="{ classPrefix: 't' }">
-    <t-space direction="vertical" style="width: 100%; text-align: center">
-      <logo />
-      <h3>Welcome to use TDesign！</h3>
-      <t-space>
-        <t-button theme="primary">
-          <add-icon />
-          新建
-        </t-button>
-        <t-button variant="outline">
-          <cloud-upload-icon />
-          上传文件
-        </t-button>
-        <t-button shape="circle" theme="primary">
-          <discount-icon />
-        </t-button>
-        <t-button shape="circle" theme="primary">
-          <cloud-download-icon />
-        </t-button>
-        <t-button variant="outline"> 搜索 </t-button>
-      </t-space>
-    </t-space>
-  </t-config-provider>
+  <t-card bordered class="card">
+    <t-form class="form">
+      <t-tabs :default-value="1" class="tab">
+        <t-tab-panel :value="1" label="User"><br>
+          <t-form-item label="User">
+            <t-input-adornment prepend="github.com/">
+              <t-input placeholder="username" />
+            </t-input-adornment>
+          </t-form-item>
+        </t-tab-panel>
+        <t-tab-panel :value="2" label="Repo"><br>
+          <t-form-item label="User">
+            <t-input-adornment prepend="github.com/">
+              <t-input placeholder="username" />
+            </t-input-adornment>
+          </t-form-item>
+          <t-form-item label="Repo">
+            <t-input placeholder="repository" />
+          </t-form-item>
+        </t-tab-panel>
+        <t-tab-panel :value="3" label="Release">
+        </t-tab-panel>
+      </t-tabs>
+      <t-form-item label="Theme" name="theme" initial-data="dark">
+        <t-select defaultValue="light">
+          <t-option key="light" label="light" value="light" />
+          <t-option key="dark" label="dark" value="dark" />
+        </t-select>
+      </t-form-item>
+    </t-form>
+    <t-button id="generate">Generate</t-button>
+  </t-card>
 </template>
 
-<script setup lang="ts">
-import {
-  AddIcon,
-  CloudUploadIcon,
-  DiscountIcon,
-  CloudDownloadIcon,
-} from "tdesign-icons-vue-next";
-import Logo from "../assets/svg/assets-t-logo.svg?component";
-</script>
+<style>
+html, body {
+  padding: 0;
+  margin: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 8px;
+  width: 100%;
+  height: 100%;
+  overflow: auto;
+  background: #fafcf1;
+}
 
+* {
+  font-family: 'Poppins', 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+}
+#app {
+  position: absolute;
+  top: 8px;
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+  margin: 30px;
+  width: 80%;
+  min-width: 220px;
+  max-width: 550px;
+}
+
+.card {
+  width: 100%;
+  display: grid;
+  grid-gap: 46px;
+}
+
+.form {
+  width: 100%;
+}
+
+.tab {
+  width: 100%;
+  margin: 20px 0;
+}
+
+#generate {
+  margin: 0 auto;
+}
+
+#preview {
+  width: 100%;
+}
+
+.quote {
+  width: 20px;
+  color: black !important;
+  float: right;
+  text-decoration: none;
+  transform: translate(5px, -5px);
+}
+</style>
--- gui/src/main.ts
+++ gui/src/main.ts
@@ -1,6 +1,5 @@
 import { createApp } from 'vue'
 import TDesign from 'tdesign-vue-next';
-import 'tdesign-vue-next/es/style/index.css';
 
 import App from './App.vue'
 
