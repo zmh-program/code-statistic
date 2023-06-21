ab8241cd1f4789252d8e07ff2700ad9ca496a99d
Zhang Minghan <zmh13054618081@dingtalk.com>
2023-06-21 17:24:18 +0800

    fix mobile adapter

--- gui/src/App.vue
+++ gui/src/App.vue
@@ -4,6 +4,7 @@ import {reactive, ref} from "vue";
 const link = ref('');
 const tab = ref('user');
 const theme = ref('light');
+const loading = ref(true);
 
 const user = reactive({
   username: '',
@@ -39,6 +40,7 @@ function validate(map: Record<string, any>) {
       return false;
     }
   }
+  loading.value = true;
   return true;
 }
 
@@ -49,6 +51,7 @@ function tabChanged(value: string) {
 function join(src: string) {
   return `https://stats.deeptrain.net` + src;
 }
+
 function generate() {
   switch (tab.value) {
     case 'user':
@@ -163,13 +166,11 @@ function generate() {
     <a class="quote" href="https://github.com/zmh-program/code-statistic">
       <svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><title>GitHub</title><path d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12"/></svg>
     </a>
-  </t-card>
-  <t-card class="response">
-    <template v-if="link">
-      <t-input v-model="link" readonly />
-      <img :src="link" alt="">
-    </template>
-    <t-loading class="loading" v-else />
+    <t-card class="response">
+      <t-loading class="loading" v-if="loading" />
+      <t-input v-else v-model="link" readonly />
+      <img :src="link" alt="" @load="loading = false">
+    </t-card>
   </t-card>
 </template>
 
@@ -230,11 +231,14 @@ html, body {
 }
 
 .response {
+  width: 100%;
   min-height: 120px;
   height: max-content;
-  margin-bottom: 24px;
 }
 
+.response .t-card__body {
+  grid-gap: 2px;
+}
 .response .loading {
   position: absolute;
   top: 50%;
